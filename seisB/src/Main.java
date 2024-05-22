51bb696c18c09fd25bd66523396eb93f57b9a08a
G-Conde <germanconde79@gmail.com>
2024-05-22 20:07:02 -0300

    inicio

--- seisB/src/Main.java
+++ seisB/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese un texto");
+        String text = scanner.nextLine();
+
+        Map<Character,Integer> frecuentlyCharacters = new HashMap<>();
+
+        for (char c : text.toCharArray()){
+            if(frecuentlyCharacters.containsKey(c)){
+                frecuentlyCharacters.put(c, frecuentlyCharacters.get(c));
+                
+
+
+            }
+        }
+
     }
 }
+
+/*Cree un programa que solicite al usuario que ingrese una cadena de texto y
+luego cuente el número de veces que cada carácter aparece en la cadena.
+Utilice un HashMap para almacenar los resultados y muestre el mapa al
+usuario al final.*/
+
---
+++ seisB/src/People.java
@@ -0,0 +1,74 @@
+public class People {
+    private String name = null;
+    private Integer age = 0;
+    private Integer dni;
+    private Character genre = 'h';
+    private Double weigth = 0.0;
+    private Double heigth = 0.0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+
+    public Character getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Character genre) {
+        this.genre = genre;
+    }
+
+    public Double getWeigth() {
+        return weigth;
+    }
+
+    public void setWeigth(Double weigth) {
+        this.weigth = weigth;
+    }
+
+    public Double getHeigth() {
+        return heigth;
+    }
+
+    public void setHeigth(Double heigth) {
+        this.heigth = heigth;
+    }
+
+    public People() {};
+
+    public People(String name, Integer age, Character genre) {
+        this.name = name;
+        this.age = age;
+        this.genre = genre;
+    }
+
+    public People(String name, Integer age, Integer dni, Character genre, Double weigth, Double heigth) {
+        this.name = name;
+        this.age = age;
+        this.dni = dni;
+        this.genre = genre;
+        this.weigth = weigth;
+        this.heigth = heigth;
+    }
+}
+
